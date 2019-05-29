e49a92fadbc5a9e4ce95a2f766a80b307b28abf3
Wouter Jansen <wouter97@planet.nl>
2019-05-29 14:48:04 +0200

    Initial, new version

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float YIncrement;
+    public float speed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic movement code; should be updated for smoother movement
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + YIncrement);
+        } else if ( Input.GetKey(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - YIncrement);
+        } else if ( Input.GetKey(KeyCode.LeftArrow))
+        {
+            targetPos = new Vector2(transform.position.x - YIncrement, transform.position.y);
+        }
+         else if ( Input.GetKey(KeyCode.RightArrow))
+        {
+            targetPos = new Vector2(transform.position.x + YIncrement, transform.position.y);
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,18 @@
+### ArenaDefense
+
+
+#### Unity version used: 2019.2.0b2
+
+#### Trello link
+https://trello.com/b/NGScU55N/arenadefense
+
+##### The game so far:
+
+ - Top-down wave defense
+ - Classes based on studies in AE&I
+ - Enemies based on problems in a study
+ 
+#### Features:
+ - Local multiplayer, up to 4 players
+ - Classes with powers based on studies
+ - Upgrades [optional]
