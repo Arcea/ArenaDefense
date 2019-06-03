3c39e7813d7d7ae6f375ec144f9a40f6a9f4e0b0
Thom Martens <thommartens14@hotmail.com>
2019-06-03 16:10:19 +0200

    added twinstick shooter controllers and basic shooting

---
+++ Assets/Scripts/PistolBulletController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PistolBulletController : MonoBehaviour
+{
+    public float zAxis;
+    private float speed = 0.4f;
+
+    public Vector2 bulletDirection;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += transform.forward * Time.deltaTime * speed;
+    }
+}
