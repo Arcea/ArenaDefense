32aded0db86f33e37739768cf0e8999d115c572f
Wouter Jansen <wouter97@planet.nl>
2019-06-03 15:07:54 +0200

    CharSelect starts game, players based on controllers

--- Assets/Scripts/CharSelectController.cs
+++ Assets/Scripts/CharSelectController.cs
@@ -9,8 +9,11 @@ public class CharSelectController : MonoBehaviour
     public string Player2Join = "Fire1_P2";
     public string Player3Join = "Fire1_P3";
     public string Player4Join = "Fire1_P4";
+    public string gameStart = "Start";
 
-    //Cleanup
+//Sidenote, should clean this up. Perhaps later
+
+    //Text
     public Text Player1Text;
     public Text Player2Text;
     public Text Player3Text;
@@ -19,10 +22,14 @@ public class CharSelectController : MonoBehaviour
     //Controllers
     private bool Controller1 = false;
     private bool Controller2 = false;
+    private bool Controller3 = false;
+    private bool Controller4 = false;
 
     //Player slots
     private bool Player1Slot = false;
     private bool Player2Slot = false;
+    private bool Player3Slot = false;
+    private bool Player4Slot = false;    
     
     // Start is called before the first frame update
     void Start()
@@ -32,64 +39,74 @@ public class CharSelectController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        // if (Input.GetMouseButtonDown(8))
-        // {
-        //     Debug.Log("Starting game..");
-        //     //UnityEngine.SceneManagement.SceneManager.LoadScene(1);
-        // }
+        //Start game
+        if (Input.GetButtonDown(gameStart))
+        {
+            Debug.Log("Starting game..");
+            UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+        }
 
-        // for (int i = 0; i < 4; i++)
-        // {
-        //     if (Mathf.Abs(Input.GetAxis("Joy" + i + "X")) > 0.2 ||
-        //         Mathf.Abs(Input.GetAxis("Joy" + i + "Y")) > 0.2)
-        //     {
-        //         Debug.Log(Input.GetJoystickNames()[i] + " is moved");
-        //     }
-        // }
-        
-
-        if(Input.GetButtonDown(Player1Join) || !Controller1)
+        //Controller check
+        if(Input.GetButtonDown(Player1Join) && !Controller1)
         {
             JoinPlayer();
             Controller1 = true;
-        } else if (Input.GetButton(Player2Join) || !Controller2)
+            Debug.Log("Controller 1 Joined");
+        } else if (Input.GetButton(Player2Join) && !Controller2)
         {
             JoinPlayer();
             Controller2 = true;
+            Debug.Log("Controller 2 Joined");
+        }  else if (Input.GetButton(Player3Join) && !Controller3)
+        {
+            JoinPlayer();
+            Controller3 = true;
+            Debug.Log("Controller 3 Joined");
+        }  else if (Input.GetButton(Player3Join) && !Controller4)
+        {
+            JoinPlayer();
+            Controller4 = true;
+            Debug.Log("Controller 4 Joined");
         }
-
-        // if(Input.GetButtonDown(Player1Join)){
-        //     Debug.Log("Player 1 Joined");
-        //     Player1Text.text = "Player 1 Joined!";
-        // }
-        // if(Input.GetButtonDown(Player2Join)){
-        //     Debug.Log("Player 2 Joined");
-        //     Player2Text.text = "Player 2 Joined!";
-        // }
-        // if(Input.GetButtonDown(Player3Join)){
-        //     Debug.Log("Player 3 Joined");
-        //     Player3Text.text = "Player 3 Joined!";
-        // }
-        // if(Input.GetButtonDown(Player4Join)){
-        //     Debug.Log("Player 4 Joined");
-        //     Player4Text.text = "Player 4 Joined!";
-        // }
+        //Controller check ready
+        if(Input.GetButtonDown(Player1Join) && Controller1)
+        {
+            Debug.Log("Controller 1 Ready");
+        } else if (Input.GetButton(Player2Join) && Controller2)
+        {
+            Debug.Log("Controller 2 Ready");
+        }  else if (Input.GetButton(Player3Join) && Controller3)
+        {
+            Debug.Log("Controller 3 Ready");
+        }  else if (Input.GetButton(Player3Join) && Controller4)
+        {
+            Debug.Log("Controller 4 Ready");
+        }
     }
 
+    //Players
     void JoinPlayer()
     {
         if (Player1Slot == false)
         {
             Debug.Log("Player 1 filled");
-            Debug.Log(Input.GetJoystickNames() + " is moved");
             Player1Slot = true;
-        }
-        else if (Player2Slot == false)
+            Player1Text.text = "Player 1 Joined!";
+        } else if (Player2Slot == false)
         {
             Debug.Log("Player 2 filled");
             Player2Slot = true;
+            Player2Text.text = "Player 2 Joined!";
+        } else if (Player3Slot == false)
+        {
+            Debug.Log("Player 3 filled");
+            Player3Slot = true;
+            Player3Text.text = "Player 3 Joined!";
+        } else if (Player4Slot == false)
+        {
+            Debug.Log("Player 4 filled");
+            Player4Slot = true;
+            Player4Text.text = "Player 4 Joined!";
         }
-        Debug.Log(Input.GetButtonDown(Player1Join));
-        Debug.Log(Input.GetButtonDown(Player2Join));
     }
 }
