ea0ae46fe12c559d9e2b0eeb1066b9a440ca5f17
thijmenboot <thijmenboot@outlook.com>
2019-06-06 14:38:07 +0200

    Inputs for character select screen

---
+++ Assets/CharacterSelectController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class CharacterSelectController : MonoBehaviour
+{
+    private Player[] players;
+    private bool allReady
+    {
+        get
+        {
+            foreach (Player player in players)
+            {
+                if (!player.IsReady() && player.GetController() != -1)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+    
+    void Start()
+    {
+        players = FindObjectsOfType<Player>().OrderBy(p => p.playerNumber).ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (allReady)
+        {
+            foreach (Player player in players)
+            {
+                if(player.PressedStart()) UnityEngine.SceneManagement.SceneManager.LoadScene(1);
+            }
+        }
+    }
+
+
+}
---
+++ Assets/PlayerAssigner.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerAssigner : MonoBehaviour
+{
+    public string joinString = "ButtonA_P";
+
+    private Player[] players;
+    private List<int> controllers = new List<int>();
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        players = FindObjectsOfType<Player>().OrderBy(p => p.playerNumber).ToArray();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 1; i <= 4; i++)
+        {
+            if (controllers.Contains(i))
+            {
+                // todo: go ready
+                continue;
+            }
+
+            if (Input.GetButtonDown(joinString + i)){
+
+                AssignController(i);
+                break;
+            }
+        }
+    }
+
+    void AssignController(int controller)
+    {
+        Debug.Log(controllers.ToString());
+        for (int i = 0; i < players.Length; i++)
+        {
+            if(players[i].GetController() == -1)
+            {
+                players[i].SetController(i);    
+                controllers.Add(controller);
+                break;
+            }
+        }
+    }
+}
