01d1a363e426d9b55213ca6e9d3cb4b55569860f
Wouter Jansen <wouter97@planet.nl>
2019-06-12 10:53:52 +0200

    Started work on waves

---
+++ Assets/Scripts/WaveController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WaveController : MonoBehaviour
+{
+    private int currentWave = 0;
+    private int EnemyNumbers = 0;
+    public GameObject spawner;
+
+    //TODO: Add Score based on enemies
+    //TODO: Add max bas on spawner
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        NextWave();
+    }
+     
+    // Update is called once per frame
+    void Update()
+    {
+        //Formula wavenumber * players or smth like that
+        Debug.Log("Current wave: " + currentWave + " Enemy number: " + EnemyNumbers);
+    }
+
+    void NextWave()
+    {
+        currentWave++;
+
+
+    }
+}
